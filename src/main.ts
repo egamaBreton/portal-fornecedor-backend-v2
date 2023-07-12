1f01f1337f34ab7a81e58fd830a0b66acc7376bd
Elivelton Gama <egama-ext@imperio.com.br>
2023-07-12 13:18:37 -0300

    new deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'
+
+dotenv.config() 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(`The connection URL is ${process.env.DATABASE_URL}`)
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
